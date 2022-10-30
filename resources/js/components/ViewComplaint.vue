17be36ffe562166d1e57ede9ddde2fcef564fd18
Jude Kikuyu <jkikuyu@gmail.com>
2022-10-31 00:08:05 +0300

    add viewcomplaint

---
+++ resources/js/components/ViewComplaint.vue
@@ -0,0 +1,45 @@
+<template>
+      <div
+        ref="modal"
+        class="modal fade"
+        id="viewcomplaint"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="ViewComplaintFormLabel"
+        aria-hidden="true"
+    >
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="ViewComplaintFormLabel">
+                    <slot name="formname" />
+                </h5>
+            </div>
+            <div class="modal-body">
+                Testing
+            </div>
+        </div>
+    </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:"ViewComplaint",
+    props: {
+        showModal: Boolean,
+    },
+    watch: {
+        showModal: function (val) {
+            if (val === true) {
+                this.modalActive();
+            }
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
