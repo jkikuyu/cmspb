2da43eb2b00742decf3144cd11d760edeabd8f4d
Jude Kikuyu <jkikuyu@gmail.com>
2022-11-08 09:34:08 +0300

    document title

---
+++ resources/js/components/ReportTitle.vue
@@ -0,0 +1,23 @@
+<template>
+    <h2 class="fs-2 mb-2 fw-bold text-center">
+        {{ pagetitle }}
+    </h2>
+
+</template>
+
+<script>
+export default {
+    name: "ReportTitle",
+    props:{
+        pagetitle: {
+            default: "Report Incident",
+            type: String
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
