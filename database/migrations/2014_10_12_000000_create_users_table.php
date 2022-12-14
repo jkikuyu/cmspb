6eab9ae4b50a2be3d904fb8c8f0df55442d871f7
Jude Kikuyu <jkikuyu@gmail.com>
2022-12-14 13:54:53 +0300

    remove unique email

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname',256)->nullable();
-            $table->string('middlename',256)->nullable();
-            $table->string('lastname',256)->nullable();
-            $table->string('userid',10)->unique();
-            $table->string('email')->unique();
+            $table->string('firstname', 256)->nullable();
+            $table->string('middlename', 256)->nullable();
+            $table->string('lastname', 256)->nullable();
+            $table->string('userid', 10)->unique();
+            $table->string('email')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
